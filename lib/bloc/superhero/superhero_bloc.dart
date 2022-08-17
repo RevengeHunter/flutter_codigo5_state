08f74944276da4779dad0a77a3228d6be0d29df3
RevengeHunter <cdtvladimir0992@gmail.com>
2022-08-16 21:33:56 -0500

    bloc 2

--- lib/bloc/superhero/superhero_bloc.dart
+++ lib/bloc/superhero/superhero_bloc.dart
@@ -1,15 +1,37 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
+import 'package:flutter_codigo_state/models/superhero_model.dart';
 import 'package:meta/meta.dart';
 
 part 'superhero_event.dart';
+
 part 'superhero_state.dart';
 
 class SuperheroBloc extends Bloc<SuperheroEvent, SuperheroState> {
-  SuperheroBloc() : super(SuperheroInitial()) {
-    on<SuperheroEvent>((event, emit) {
-      // TODO: implement event handler
+  SuperheroBloc() : super(SuperheroState()) {
+    on<SuperheroCreateEvent>((event, emit) {
+      emit(SuperheroState(superheroModel: event.superheroModel));
+    });
+
+    on<SuperheroUpdateExperienceEvent>((event, emit) {
+      emit(
+        SuperheroState(
+          superheroModel: state.superheroModel!.copyWith(
+            newExperience: event.experience,
+          ),
+        ),
+      );
+    });
+
+    on<SuperheroAddSkillEvent>((event, emit) {
+      List<String> skills = state.superheroModel!.skills;
+      skills.add(event.skill);
+      emit(
+        SuperheroState(
+          superheroModel: state.superheroModel!.copyWith(
+            newSkills: skills,
+          ),
+        ),
+      );
     });
   }
 }
--- lib/bloc/superhero/superhero_event.dart
+++ lib/bloc/superhero/superhero_event.dart
@@ -2,3 +2,18 @@ part of 'superhero_bloc.dart';
 
 @immutable
 abstract class SuperheroEvent {}
+
+class SuperheroCreateEvent extends SuperheroEvent {
+  SuperheroModel superheroModel;
+  SuperheroCreateEvent({required this.superheroModel});
+}
+
+class SuperheroUpdateExperienceEvent extends SuperheroEvent {
+  int experience;
+  SuperheroUpdateExperienceEvent({required this.experience,});
+}
+
+class SuperheroAddSkillEvent extends SuperheroEvent {
+  String skill;
+  SuperheroAddSkillEvent({required this.skill});
+}
